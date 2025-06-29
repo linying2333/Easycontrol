1712415b2ec5e2d4d3a229230a3553127095b2aa
linying2333 <106875506+linying2333@users.noreply.github.com>
2025-06-29 13:19:40 +0800

    Update ActiveActivity.java
    remove isActive

--- easycontrol/app/src/main/java/top/saymzx/easycontrol/app/ActiveActivity.java
+++ easycontrol/app/src/main/java/top/saymzx/easycontrol/app/ActiveActivity.java
@@ -1,80 +1,48 @@
 package top.saymzx.easycontrol.app;
 
 import android.app.Activity;
-import android.app.Dialog;
 import android.os.Bundle;
-import android.util.Pair;
 import android.view.View;
-import android.view.WindowManager;
 
 import top.saymzx.easycontrol.app.databinding.ActivityActiveBinding;
-import top.saymzx.easycontrol.app.databinding.ItemLoadingBinding;
-import top.saymzx.easycontrol.app.entity.AppData;
-import top.saymzx.easycontrol.app.helper.ActiveHelper;
 import top.saymzx.easycontrol.app.helper.PublicTools;
 import top.saymzx.easycontrol.app.helper.ViewTools;
 
 public class ActiveActivity extends Activity {
 
-  private ActivityActiveBinding activityActiveBinding;
+    private ActivityActiveBinding activityActiveBinding;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    ViewTools.setStatusAndNavBar(this);
-    ViewTools.setLocale(this);
-    activityActiveBinding = ActivityActiveBinding.inflate(this.getLayoutInflater());
-    setContentView(activityActiveBinding.getRoot());
-    // 取消激活
-    if (AppData.setting.getIsActive()) deactivate();
-    setButtonListener();
-    // 绘制UI
-    drawUi();
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ViewTools.setStatusAndNavBar(this);
+        ViewTools.setLocale(this);
+        activityActiveBinding = ActivityActiveBinding.inflate(this.getLayoutInflater());
+        setContentView(activityActiveBinding.getRoot());
+        
+        // 设置UI
+        setUi();
+    }
 
-  private void drawUi() {
-    activityActiveBinding.key.setText(AppData.setting.getActiveKey());
-    activityActiveBinding.url.setOnClickListener(v -> PublicTools.startUrl(this, "https://gitee.com/mingzhixianweb/easycontrol/blob/master/DONATE.md"));
-  }
+    private void setUi() {
+        // 显示激活密钥
+        activityActiveBinding.key.setText(AppData.setting.getActiveKey());
+        
+        // 设置捐赠链接点击事件
+        activityActiveBinding.url.setOnClickListener(v -> 
+            PublicTools.startUrl(this, "https://gitee.com/mingzhixianweb/easycontrol/blob/master/DONATE.md"));
+        
+        // 移除激活按钮功能
+        activityActiveBinding.active.setVisibility(View.GONE);
+        
+        // 添加关闭按钮
+        activityActiveBinding.closeButton.setVisibility(View.VISIBLE);
+        activityActiveBinding.closeButton.setOnClickListener(v -> finish());
+    }
 
-  private void setButtonListener() {
-    activityActiveBinding.active.setOnClickListener(v -> {
-      String activeKey = String.valueOf(activityActiveBinding.key.getText());
-      AppData.setting.setActiveKey(activeKey);
-      Pair<ItemLoadingBinding, Dialog> loading = ViewTools.createLoading(this);
-      loading.second.show();
-      new Thread(() -> {
-        boolean isOk = ActiveHelper.active(activeKey);
-        loading.second.cancel();
-        AppData.uiHandler.post(() -> {
-          if (isOk) {
-            finish();
-            AppData.setting.setIsActive(true);
-            PublicTools.startUrl(this, "https://gitee.com/mingzhixianweb/easycontrol/blob/master/HOW_TO_USE.md");
-            PublicTools.logToast("active", getString(R.string.toast_success), true);
-          } else PublicTools.logToast("active", getString(R.string.toast_fail), true);
-        });
-      }).start();
-    });
-  }
-
-  // 取消激活
-  private void deactivate() {
-    Pair<ItemLoadingBinding, Dialog> loading = ViewTools.createLoading(this);
-    loading.second.show();
-    new Thread(() -> {
-      boolean isOk = ActiveHelper.deactivate(AppData.setting.getActiveKey());
-      loading.second.cancel();
-      AppData.uiHandler.post(() -> {
-        if (isOk) {
-          AppData.setting.setIsActive(false);
-          PublicTools.logToast("deactivate", getString(R.string.toast_success), true);
-        } else PublicTools.logToast("deactivate", getString(R.string.toast_fail), true);
-      });
-    }).start();
-  }
-
-  @Override
-  public void onBackPressed() {
-  }
+    @Override
+    public void onBackPressed() {
+        // 允许返回
+        finish();
+    }
 }
